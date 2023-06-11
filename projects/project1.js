fd4ed3b88f73d265231457429ae1c5d61e1c1345
Sherah <sherah_israel@outlook.com>
2023-06-11 11:02:49 +0100

    Background Parallax effect added

---
+++ projects/project1.js
@@ -0,0 +1,18 @@
+var tabButtons = document.querySelectorAll(".tabs .title button");
+var tabContent = document.querySelectorAll(".tabs .content");
+
+function showContent(contentIndex,colorCode) {
+    tabButtons.forEach(function(node) {
+        node.style.backgroundColor = "";
+        node.style.color = ""
+    });
+    tabButtons[contentIndex].style.backgroundColor = colorCode;
+    tabButtons[contentIndex].style.color = "brown";
+    tabContent.forEach(function(node) {
+        node.style.display = "none";
+    });
+    tabContent[contentIndex].style.display = "block";
+    tabContent[contentIndex].style.backgroundColor = colorCode;
+}
+
+showContent(0, '#fff');
